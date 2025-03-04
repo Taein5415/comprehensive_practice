627608c72844b1275e97b5ffff4ca423668a5046
Taein5415 <kskk5050@naver.com>
2025-03-04 17:50:13 +0900

    feat: DrinkService 기능 구현

--- src/main/java/com/taein/comprehensive_practice/service/DrinkService.java
+++ src/main/java/com/taein/comprehensive_practice/service/DrinkService.java
@@ -12,8 +12,21 @@ public class DrinkService {
         this.drinkRepository = drinkRepository;
     }
 
-    public static void buyDrink(String name) {
-        
+    public void buyDrink(String name) {
+        Drink drink;
+        if((drink = drinkRepository.selectByname(name))==null){
+            throw new IllegalArgumentException("해당 음료는 존재하지 않습니다.");
+        }
+        if(drink.getQuantity() <1){
+            throw new IllegalArgumentException("해당 음료의 재고가 없습니다.");
+        }
+        drink.setQuantity(drink.getQuantity()-1);
+        drinkRepository.updateDrink(drink);
+
+    }
+
+    private static boolean drinkNotExists(String name) {
+
     }
 
     public List<Drink> findAllDrinks(){
