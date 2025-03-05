b13018395f90358456df98faf5f65140134a6ef1
Taein5415 <kskk5050@naver.com>
2025-03-05 23:49:00 +0900

    chore: 메서드 명 변경

--- src/main/java/com/taein/comprehensive_practice/repository/DrinkRepository.java
+++ src/main/java/com/taein/comprehensive_practice/repository/DrinkRepository.java
@@ -5,10 +5,16 @@ import com.taein.comprehensive_practice.domain.Drink;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class DrinkRepository {
     private final Storage fileDrinkStorage;
     private final List<Drink> drinkList;
+
+    public void save(Drink drink){
+        drinkList.add(drink);
+        fileDrinkStorage.save(drinkList);
+    }
 
     public DrinkRepository(FileDrinkStorage fileDrinkStorage){
         this.fileDrinkStorage = fileDrinkStorage;
@@ -20,8 +26,16 @@ public class DrinkRepository {
     }
 
 
-    public Drink selectByname(String name) {
-        return (Drink) drinkList.stream().filter(d -> d.getName().equals(name));
+    public Drink selectByName(String name) {
+        Optional<Drink> drink = drinkList.stream()
+                .filter(d -> d.getName().equals(name))
+                .findFirst(); // 첫 번째 일치하는 요소 반환
+
+        // 값이 존재하면 출력, 없으면 메시지 출력
+        if(drink.isPresent())
+            return drink.get();
+        else
+            throw new NullPointerException("해당 음료는 존재하지 않습니다.");
     }
 
     public void updateDrink(Drink drink) {
