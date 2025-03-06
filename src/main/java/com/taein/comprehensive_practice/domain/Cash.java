877e6e37d447a8b3cbb6ac7519aa379d849b3c62
Taein5415 <kskk5050@naver.com>
2025-03-06 17:36:57 +0900

    refactor: Serializable 상속 및 필드명 변경

--- src/main/java/com/taein/comprehensive_practice/domain/Cash.java
+++ src/main/java/com/taein/comprehensive_practice/domain/Cash.java
@@ -1,13 +1,40 @@
 package com.taein.comprehensive_practice.domain;
 
-public class Cash {
-    private String name;
-    private String value;
+import java.io.Serializable;
+
+public class Cash implements Serializable {
+    private int unit;
     private int quantity;
 
-    public Cash(String name, String value, int quantity) {
-        this.name = name;
-        this.value = value;
+    public Cash(int unit, int quantity) {
+        this.unit = unit;
         this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return unit +"원 : "+quantity+"개";
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void add(int num){
+        this.quantity+=num;
+    }
+    public void minus(int num){
+        this.quantity-=num;
     }
 }
--- src/main/java/com/taein/comprehensive_practice/repository/FileCashStorage.java
+++ src/main/java/com/taein/comprehensive_practice/repository/FileCashStorage.java
@@ -9,6 +9,17 @@ import java.util.List;
 
 public class FileCashStorage implements Storage<Cash> {
     private final static String FILE_PATH = "src/main/java/com/taein/comprehensive_practice/db/cashDB.dat";
+
+    private static FileCashStorage instance;
+    private FileCashStorage(){}
+
+    public static FileCashStorage getInstance(){
+        if (instance == null) {
+            instance = new FileCashStorage();
+        }
+        return instance;
+    }
+
     @Override
     public void save(List<Cash> list) {
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
@@ -23,11 +34,17 @@ public class FileCashStorage implements Storage<Cash> {
     public List<Cash> load() {
         File file = new File(FILE_PATH);
         if(!file.exists())
-            return new ArrayList<Cash>();
+            return new ArrayList<Cash>(List.of(
+                    new Cash(50,0),
+                    new Cash(100,0),
+                    new Cash(500,0),
+                    new Cash(1000,0),
+                    new Cash(5000,0)
+            ));
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
             return (List<Cash>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException("drinkDB.txt 파일 읽기 오류");
+            throw new RuntimeException("cashDB.txt 파일 읽기 오류");
         }
     }
 }
